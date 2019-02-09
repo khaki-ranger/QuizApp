50c56f4b3eaadf65b5c33ef194dcce3403e68baf
Terashima <yamanogomora@gmail.com>
2019-02-09 12:44:42 +0900

    結果画面のクラスを実装

--- QuizApp/ResultViewController.swift
+++ QuizApp/ResultViewController.swift
@@ -10,10 +10,31 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    // 正答率のラベル
+    @IBOutlet weak var correctPercentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // 問題数を取得する
+        let questionCount = QuestionDataManager.sharedInstance.questionDataArray.count
+        
+        // 正答数を取得する
+        var correctCount: Int = 0
+        
+        // 正答数を計算する
+        for questionData in QuestionDataManager.sharedInstance.questionDataArray {
+            if questionData.isCorrect() {
+                // 正答数を増やす
+                correctCount += 1
+            }
+        }
+        
+        // 正答率を計算する
+        let correctPercent: Float = (Float(correctCount) / Float(questionCount)) * 100
+        
+        // 正答率を少数第一位まで計算して画面に反映する
+        correctPercentLabel.text = String(format: "%.1f", correctPercent) + "%"
     }
 
     override func didReceiveMemoryWarning() {
